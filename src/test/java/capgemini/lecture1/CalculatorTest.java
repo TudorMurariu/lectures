56f0de7bbe250171ebb278915524bff4946e95da
Tudor <tudorcristianmurariu@yahoo.com>
2022-08-26 13:14:06 +0300

    Added Tests for add and dif (for the calculator)

--- src/test/java/capgemini/lecture1/CalculatorTest.java
+++ src/test/java/capgemini/lecture1/CalculatorTest.java
@@ -15,6 +15,7 @@ class CalculatorTest {
         calculator = new Calculator();
     }
 
+    /// Sum function
     @Test
     @DisplayName("When we add two positive numbers we receive positive number")
         //Given-When-Then
@@ -36,4 +37,44 @@ class CalculatorTest {
         assertTrue(calculator.sum(-11, -22) < 0);
     }
 
+    @Test
+    @DisplayName("When we add two big Negative numbers we receive Negative number")
+        //Given-When-Then
+    void sum_twoBigPositiveNumbers_NegativeNumber() {
+        assertTrue(calculator.sum(-Integer.MAX_VALUE, -Integer.MAX_VALUE) < 0);
+    }
+
+    // Dif function
+
+    @Test
+    @DisplayName("We subtract a small number from a big positive number")
+        //Given-When-Then
+    void dif_oneBiggerPositiveAndOneSmaller_positive() {
+        assertTrue(calculator.dif(7, 1) > 0);
+        assertTrue(calculator.dif(1002, 777) > 0);
+        assertTrue(calculator.dif(898, 88) > 0);
+    }
+
+    @Test
+    @DisplayName("We subtract a smaller negative number from a big negative number")
+        //Given-When-Then
+    void dif_oneBiggerNegativeAndOneSmaller_positive() {
+        assertTrue(calculator.dif(-7, -9) > 0);
+        assertTrue(calculator.dif(-1002, -20000) > 0);
+        assertTrue(calculator.dif(-8, -88879) > 0);
+    }
+
+    @Test
+    @DisplayName("We subtract a big positive from a small positive")
+    void dif_oneSmallPositiveAndOneBigPositive_negative() {
+        assertTrue(calculator.dif(9, 90) < 0);
+        assertTrue(calculator.dif(132, 1243546) < 0);
+    }
+
+    @Test
+    @DisplayName("We subtract a big negative from a small negative")
+    void dif_eadwdwaadawaawdawwddadw() {
+        assertTrue(calculator.dif(-90, -9) > 0);
+        assertTrue(calculator.dif(-131243546,-77) > 0);
+    }
 }
